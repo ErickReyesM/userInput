97c9d72e0f216faaf2a70edce8e7dfed98795851
Erick Reyes <erick.reyesm04@gmail.com>
2019-02-19 19:54:15 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'userInput';
+export class AppComponent implements OnInit {
+  surveysInDB: any[] = [];
+  pageSize:number = 10;
+  isLoadingResults:boolean;
+  collectionFB:string = 'surveys';
+
+  constructor() { }
+
+  ngOnInit() {
+    this.isLoadingResults = true;
+    this.GetPost();
+  }
+
+  GetPost(){
+    this.surveysInDB = [];
+    firebase.firestore().collection(this.collectionFB).orderBy("created","desc").limit(this.pageSize).get()
+    .then((docs)=>{
+      docs.forEach((doc)=>{
+        this.surveysInDB.push(doc);
+      })
+      this.isLoadingResults = false;
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+
+  onOpenSurvey(doc:any){
+    console.log(doc.id);
+  }
+
+  ago(time){
+    moment.locale("es-us")
+    let difference = moment(time).diff(moment());
+    return moment.duration(difference).humanize();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatButtonModule, MatProgressSpinnerModule } from '@angular/material';
+import * as firebase from 'firebase';
+var config = {
+  apiKey: "AIzaSyCMsiQRCVEa-qzxA-bnt-Jk5FXyOQgFrBw",
+  authDomain: "sondaggio-ea6bf.firebaseapp.com",
+  databaseURL: "https://sondaggio-ea6bf.firebaseio.com",
+  projectId: "sondaggio-ea6bf",
+  storageBucket: "",
+  messagingSenderId: "22946728796"
+};
+firebase.initializeApp(config);
 
 @NgModule({
   declarations: [
@@ -10,7 +22,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
